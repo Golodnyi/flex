58602322101437c2f67cea16c974ba4e81b8c726
Golodnyi <ochen@golodnyi.ru>
2019-02-21 15:15:18 +0700

    xor sum func

--- src/flex/head.rs
+++ src/flex/head.rs
@@ -7,3 +7,15 @@ pub struct Head {
     pub csd     : u8,
     pub csp     : u8
 }
+
+pub fn xor_sum(buffer: u32, length: u32) -> u32 {
+    let temp_sum: u32 = 0;
+
+    while length > 0 {
+        buffer += 1;
+        let temp_sum: u32 = temp_sum ^ buffer;
+        length -= 1;
+    }
+
+    temp_sum
+}
